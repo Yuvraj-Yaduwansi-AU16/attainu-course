24a351e22f9ee3e6a8a27dc1ae79bb7583d4e7f2
Yuvraj Singh <yuvraj.mark3@gmail.com>
2020-11-18 10:33:44 +0530

    Added Simple Calculator Assignment of week2 day 2 of python

---
+++ coding-challenges/week02/Coding_challenge_week_2_day_2_18-Nov-2020.py
@@ -0,0 +1,32 @@
+print("Enter the first number for addition")
+num1= float(input())
+
+print("Enter the second number for addition")
+num2= float (input())
+
+print("The addition of the two numbers is" , num1+num2)
+
+
+print("Enter the first number for subtraction")
+num3= float (input())
+
+print("Enter the second number for subtraction")
+num4= float (input())
+
+print("The subtraction of the two numbers is" , num3-num4)
+
+print("Enter the first number for multiplication")
+num5=float (input())
+
+print("Enter the second number for multiplication")
+num6=float (input())
+
+print("The multiplication of the two numbers  is" , num5*num6)
+
+print("Enter the first number for division")
+num7= float (input())
+
+print("Enter the second number for division")
+num8= float(input())
+
+print("The division of the two numbers is" , num7/num8)
