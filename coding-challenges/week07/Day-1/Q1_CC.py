2cd762e25dab80ead3e37ce65c972ebe3b22c1fd
Yuvraj Singh <yuvraj.mark3@gmail.com>
2020-12-22 20:25:24 +0530

    Modified file names of CC week 7 day 1

--- coding-challenges/week07/Day-1/Q1_CC.py
+++ coding-challenges/week07/Day-1/Q1_CC.py
@@ -1,23 +1,44 @@
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        p1 = 0
-        p2 = 0
-        
-        while p1 < m and p2 < n:
-            if nums1[p1] <= nums2[p2]:
-                p1 += 1
-            else:
-                nums1.insert(p1, nums2[p2])
-                nums1.pop()
-                p2 += 1
-                p1 += 1
-                m += 1
-        
-        while p2 < n:
-            if nums1[p1] <= nums2[p2] and nums1[p1] != 0:
-                p1 += 1
-            else:
-                nums1.insert(p1, nums2[p2])
-                nums1.pop()
-                p2 += 1
-                p1 += 1
+def merge(a, start_one, end_one, start_two, end_two):
+    p1 = start_one
+    p2 = start_two
+    temp = []
+
+    while p1 <= end_one and p2 <= end_two:
+        if a[p1] < a [p2]:
+            temp.append(a[p1])
+            p1 += 1
+        else:
+            temp.append(a[p2])
+            p2 += 1
+    
+    while p1 <= end_one:
+        temp.append(a[p1])
+        p1 += 1
+
+    while p2 <= end_two:
+        temp.append(a[p2])
+        p2 += 1
+
+    i = 0
+    while i < len(temp):
+        a[i + start_one] = temp[i]
+        i += 1
+
+def mergeSort(a, left, right):
+    if left >= right:
+        return
+
+    mid = (left + right) >> 1
+    mergeSort(a, left, mid)
+    mergeSort(a, mid + 1, right)
+
+    merge(a, left, mid, mid + 1, right)
+
+if __name__ == "__main__":
+    a = [5,6,9,8,5,6,1,233,34,11,10]
+    print("Unsorted array" ,a)
+    right = len(a) - 1
+
+    mergeSort(a, 0, right)
+
+    print("sorted array" ,a)
