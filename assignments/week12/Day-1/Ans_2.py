5f5f44a100408a97428ca8270412073c42bac2eb
Yuvraj Singh <yuvraj.mark3@gmail.com>
2021-01-28 18:43:05 +0530

    Adding Assignment of w12 d1

---
+++ assignments/week12/Day-1/Ans_2.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the insertionSort1 function below.
+def insertionSort1(n, arr):
+    elem = arr[n - 1]
+    i = n - 2
+
+    while i >= 0 and elem < arr[i]:
+        arr[i + 1] = arr[i]
+        print(*arr)
+        i -= 1
+
+    arr[i + 1] = elem
+    print(*arr)
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
+    
