71e4f8a6268b5c076028320646d34a8d77748e5a
Yuvraj Singh <yuvraj.mark3@gmail.com>
2021-02-09 23:19:36 +0530

    Adding Asg of w13 d3

---
+++ assignments/week13/Day-3/Ans_1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res = set()
+        for first in range(len(nums)):
+            for second in range(first + 1, len(nums)):
+
+                third = second + 1
+                fourth = len(nums) - 1
+
+                while third < fourth:
+                    current_sum = nums[first] + nums[second] + nums[third] + nums[fourth]
+                    
+                    if target == current_sum:
+                        res.add((nums[first], nums[second], nums[third], nums[fourth]))
+                        third += 1
+                        fourth -= 1
+
+                    elif current_sum < target:
+                        third += 1
+
+                    else:
+                        fourth -= 1
+        return res
