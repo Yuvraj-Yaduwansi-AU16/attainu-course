9ab13d1cc7d03905504dbc6e40919c80ca8f82f5
Yuvraj Singh <yuvraj.mark3@gmail.com>
2021-03-11 16:29:54 +0530

    Adding cc of w17d3

---
+++ coding-challenges/week17/Day-3/script.js
@@ -0,0 +1,8 @@
+const number = parseInt(prompt('Enter an integer: '));
+
+const range = parseInt(prompt('Enter a range: '));
+
+for(let i = 1; i <= range; i++) {
+    const result = i * number;
+    console.log(`${number} * ${i} = ${result}`);
+}
