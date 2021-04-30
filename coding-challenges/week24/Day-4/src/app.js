dbfd00e26df5870f9e09698582f10e4d21bd099f
Yuvraj Singh <yuvraj.mark3@gmail.com>
2021-04-30 22:27:11 +0530

    Adding CC of w24d4

---
+++ coding-challenges/week24/Day-4/src/app.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const router = require('./routers/home')
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectoryPath));
+app.use(express.urlencoded({ extended: true }));
+app.use(router)
+
+app.listen(port, () => console.log('Server is up on port ' + port));
