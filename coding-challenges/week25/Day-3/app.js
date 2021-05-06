c8516a1a6a1572557c373bcfadaaedb115a69ac3
Yuvraj Singh <yuvraj.mark3@gmail.com>
2021-05-06 21:47:59 +0530

    Adding CC

---
+++ coding-challenges/week25/Day-3/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const expHbs = require("express-handlebars");
+const User = require("./models/User");
+
+const app = express();
+app.use(express.static("./public"));
+
+app.engine("hbs", expHbs({ extname: "hbs" }));
+app.set("view engine", "hbs");
+
+const DB_URL =
+  "mongodb+srv://express-attainu:qAmxNDPVztn2rSUG@cluster0.bg4zd.mongodb.net/yelpcamp-attainu?retryWrites=true&w=majority";
+
+mongoose.connect(
+  DB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  async (err) => {
+    if (err) throw err;
+
+    console.log("DataBase Connected");
+
+    const newUser = {
+      email: "uv@gmail.com",
+      password: "pataNahi",
+    };
+
+    const user = new User(newUser);
+
+    const result = await user.save();
+
+    console.log(result);
+  }
+);
+
+app.get("/", (req, res) => {
+  res.render("landing");
+});
+
+app.listen(3000, () => console.log("Server Started"));
