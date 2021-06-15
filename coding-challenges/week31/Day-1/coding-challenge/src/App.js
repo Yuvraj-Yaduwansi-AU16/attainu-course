9fce69477ebd8fd2e4f45c03434eeb76bd330431
Yuvraj Singh <yuvraj.mark3@gmail.com>
2021-06-15 18:17:58 +0530

    Adding CC

--- coding-challenges/week31/Day-1/coding-challenge/src/App.js
+++ coding-challenges/week31/Day-1/coding-challenge/src/App.js
@@ -1,40 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  const [Btn, setBtn] = useState(false);
-  const changeBtn = (e) => {
-    e.preventDefault();
-    setBtn(true);
-    setTimeout(() => {
-      setBtn(false);
-    }, 5000);
+class App extends Component {
+  state = {
+    disabled: false,
+    changeBtn: (e) => {
+      e.preventDefault();
+      this.setState({ disabled: true });
+      setTimeout(() => {
+        this.setState({ disabled: false });
+      }, 5000);
+    },
   };
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <input
-          type='button'
-          value='Button'
-          onClick={changeBtn}
-          disabled={Btn}
-        />
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <input
+            type='button'
+            value='Button'
+            onClick={this.state.changeBtn}
+            disabled={this.state.disabled}
+          />
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
